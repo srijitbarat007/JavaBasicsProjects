42c118a33b08bca8b5dd9a373d5d4adaf3abdb29
srijit <srijit.barat@vodafone.com>
2022-08-22 21:21:17 +0530

    JavaBasicsProject - TeenNumberChecker

---
+++ src/com/javaprojects/teennumberchecker/TeenNumberChecker.java
@@ -0,0 +1,25 @@
+package com.javaprojects.teennumberchecker;
+
+public class TeenNumberChecker {
+    public static boolean isTeen(int age){
+        if(age>=13 && age<=19)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean hasTeen(int age1, int age2, int age3){
+        if((age1>=13 && age1<=19)||(age2>=13 && age2<=19)||(age3>=13 && age3<=19))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(13));
+    }
+}
